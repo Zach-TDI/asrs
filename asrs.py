1721deccebdf200f750fa1b62c8dbacd2cb6a47d
Zach Cherrington <zncherri@gmail.com>
2020-04-17 07:40:12 +0100

    bug fixs on ASRS

--- asrs.py
+++ asrs.py
@@ -6,12 +6,13 @@ allenBradley = '192.168.1.3'
 #	ret = comm.Read('MyTagName')
 #	print(ret.TagName, ret.Value, ret.Status)
 
-addr = 0x8 # bus address
+addr = 0x8 # Jakes bus address
+#addr2 = 0xF # Zachs bus address
 bus = SMBus(1) # indicates /dev/ic2-1
 
 numb = 1
 
-#print ("Enter a hex 1 to B to designate what position you wish to move to ")
+print ("Enter a hex 1 to B to designate what position you wish to move to ")
 while numb == 1:
 
 	jakesStepper = input(">>>>   ")
@@ -24,21 +25,21 @@ while numb == 1:
 		bus.write_byte(addr, 0x2) # move from 0 - 3
 	elif jakesStepper == "3":
 		bus.write_byte(addr, 0x3) # move from 1 - 0
-	elif jakesStepper == "0":
+	elif jakesStepper == "4":
 		bus.write_byte(addr, 0x4) # move from 1 - 2
-	elif jakesStepper == "1":
+	elif jakesStepper == "5":
 		bus.write_byte(addr, 0x5) # move from 1 - 3
-	elif jakesStepper == "2":
+	elif jakesStepper == "6":
 		bus.write_byte(addr, 0x6) # move from 2 - 0
-	elif jakesStepper == "3":
+	elif jakesStepper == "7":
 		bus.write_byte(addr, 0x7) # move from 2 - 1
-	elif jakesStepper == "0":
+	elif jakesStepper == "8":
 		bus.write_byte(addr, 0x8) # move from 2 - 3
-	elif jakesStepper == "1":
+	elif jakesStepper == "9":
 		bus.write_byte(addr, 0x9) # move from 3 - 0
-	elif jakesStepper == "2":
+	elif jakesStepper == "A":
 		bus.write_byte(addr, 0xA) # move from 3 - 1
-	elif jakesStepper == "3":
+	elif jakesStepper == "B":
 		bus.write_byte(addr, 0xB) # move from 3 - 2
 	else:
 		numb = 0
